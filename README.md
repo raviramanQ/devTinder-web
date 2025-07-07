30cab8da420d59fc72c13d377524fb71c74c9768
Ravi <ravi.raman@unicorntechmedia.com>
2025-07-07 07:53:47 +0530

    7 july

--- README.md
+++ README.md
@@ -4,4 +4,17 @@
 - Remove unnecessary code and create a hello world app
 - Install Tailwind CSS
 - Install Daisy UI
-- Add NavBar Component to app.jsx file.
+- Add NavBar Component to App.jsx file.
+- Create NavBar.jsx seperate Component file
+- Install react router dom 
+- Create BrowserRouter > Router > Route =/ Body > RouteChildren
+- Create an Outlet in your Body Component
+- Create a footer
+- Create a Login Page
+
+Body 
+     NavBar
+     Route=/ => Feed
+     Route=/login => Login
+     Route=/connection => Connection
+     Route=/profile => Profile
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import axios from 'axios'
 
 const Login = () => {
+
+    const [emailId, setEmailId] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const handleLogin = async () => {
+        try{
+
+            const res = await axios.post("http://localhost:7777/login", {
+                emailId,
+                password
+            });
+        }
+        catch(e){
+            console.error(e);
+        }
+    };
+    
+
   return (
-    <div>Login</div>
-  )
-}
+    <div className='flex justify-center my-20'>
+     <div className="card bg-base-300 w-96 shadow-sm">
+     <div className="card-body">
+       <h2 className="card-title justify-center">Login</h2>
+       <div>
+       <label className="label">
+         <span className="label-text">Email ID</span>
+       </label>
+       <input type="email" placeholder="email" 
+       className="input input-bordered" 
+       value={emailId} 
+       onChange={(e) => setEmailId(e.target.value)}
+       />
+       </div>
+       <div>
+       <label className="label">
+         <span className="label-text">Password</span>
+       </label>
+       <input type="password" placeholder="password"
+        className="input input-bordered" 
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        />
+       </div>
+       <div className="card-actions justify-center">
+       <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+      </div>
+      </div>
+     </div>
+    </div>
+  );
+};
 
 export default Login
